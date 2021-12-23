7bae7ced8de403823c2e83a86f1a0745ff0fff5e
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2021-12-23 12:34:49 +0700

    [Fix] Ensure android notification repo is initialized from background thread

--- base/src/main/java/m/co/rh/id/a_flash_deck/base/provider/DatabaseProviderModule.java
+++ base/src/main/java/m/co/rh/id/a_flash_deck/base/provider/DatabaseProviderModule.java
@@ -21,12 +21,16 @@ import android.content.Context;
 
 import androidx.room.Room;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
 import m.co.rh.id.a_flash_deck.base.dao.DeckDao;
 import m.co.rh.id.a_flash_deck.base.dao.NotificationTimerDao;
 import m.co.rh.id.a_flash_deck.base.dao.TestDao;
 import m.co.rh.id.a_flash_deck.base.repository.AndroidNotificationRepo;
 import m.co.rh.id.a_flash_deck.base.room.AppDatabase;
 import m.co.rh.id.a_flash_deck.base.room.DbMigration;
+import m.co.rh.id.alogger.ILogger;
 import m.co.rh.id.aprovider.Provider;
 import m.co.rh.id.aprovider.ProviderModule;
 import m.co.rh.id.aprovider.ProviderRegistry;
@@ -49,8 +53,19 @@ public class DatabaseProviderModule implements ProviderModule {
                 provider.get(AppDatabase.class).deckDao());
         providerRegistry.registerAsync(TestDao.class, () ->
                 provider.get(AppDatabase.class).testDao());
-        providerRegistry.registerLazy(AndroidNotificationRepo.class, () ->
-                new AndroidNotificationRepo(provider.get(AppDatabase.class).androidNotificationDao()));
+        providerRegistry.registerAsync(AndroidNotificationRepo.class, () ->
+                {
+                    // ensure that this is initialized in background thread due to SQLite logic
+                    Future<AndroidNotificationRepo> androidNotificationRepoFuture = provider.get(ExecutorService.class)
+                            .submit(() -> new AndroidNotificationRepo(provider.get(AppDatabase.class).androidNotificationDao()));
+                    try {
+                        return androidNotificationRepoFuture.get();
+                    } catch (Exception e) {
+                        provider.get(ILogger.class).e("DatabaseProviderModule", "failed to init AndroidNotificationRepo", e);
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
         providerRegistry.registerLazy(NotificationTimerDao.class, () ->
                 provider.get(AppDatabase.class).timerNotificationDao());
     }
