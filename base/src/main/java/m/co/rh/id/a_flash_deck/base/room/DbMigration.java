bc4888695fec1370ec6a2524a6400d5c330b9a5f
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2021-12-23 16:32:48 +0700

    [Fix] Clear android_notification table

--- base/src/main/java/m/co/rh/id/a_flash_deck/base/room/DbMigration.java
+++ base/src/main/java/m/co/rh/id/a_flash_deck/base/room/DbMigration.java
@@ -6,7 +6,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 public class DbMigration {
     public static Migration[] getAllMigrations() {
         return new Migration[]{MIGRATION_1_2, MIGRATION_2_3,
-                MIGRATION_3_4, MIGRATION_4_5};
+                MIGRATION_3_4, MIGRATION_4_5, MIGRATION_5_6};
     }
 
     public static final Migration MIGRATION_1_2 = new Migration(1, 2) {
@@ -34,6 +34,14 @@ public class DbMigration {
     };
 
     public static final Migration MIGRATION_4_5 = new Migration(4, 5) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            // cleanup android notification that are not deleted due to bug
+            database.execSQL("DELETE FROM android_notification");
+        }
+    };
+
+    public static final Migration MIGRATION_5_6 = new Migration(5, 6) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
             // cleanup android notification that are not deleted due to bug
