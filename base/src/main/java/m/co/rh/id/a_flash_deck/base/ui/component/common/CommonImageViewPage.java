facb7b4d1d50104351c0c51b913db16042a523ac
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2022-03-05 15:38:45 +0700

    [Feature] Add back and share button when viewing image

--- base/src/main/java/m/co/rh/id/a_flash_deck/base/ui/component/common/CommonImageViewPage.java
+++ base/src/main/java/m/co/rh/id/a_flash_deck/base/ui/component/common/CommonImageViewPage.java
@@ -18,30 +18,59 @@
 package m.co.rh.id.a_flash_deck.base.ui.component.common;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
 import java.io.Serializable;
 
 import m.co.rh.id.a_flash_deck.base.R;
+import m.co.rh.id.a_flash_deck.util.UiUtils;
 import m.co.rh.id.anavigator.NavRoute;
 import m.co.rh.id.anavigator.StatefulView;
 import m.co.rh.id.anavigator.annotation.NavInject;
+import m.co.rh.id.anavigator.component.INavigator;
 
-public class CommonImageViewPage extends StatefulView<Activity> {
+public class CommonImageViewPage extends StatefulView<Activity> implements View.OnClickListener {
 
+    @NavInject
+    private transient INavigator mNavigator;
     @NavInject
     private transient NavRoute mNavRoute;
 
     @Override
     protected View createView(Activity activity, ViewGroup container) {
         ViewGroup rootLayout = (ViewGroup) activity.getLayoutInflater().inflate(R.layout.common_page_imageview, container, false);
+        Button backButton = rootLayout.findViewById(R.id.button_back);
+        backButton.setOnClickListener(this);
+        Button shareButton = rootLayout.findViewById(R.id.button_share);
+        shareButton.setOnClickListener(this);
         ImageView imageView = rootLayout.findViewById(R.id.image);
         imageView.setImageURI(getFileUri());
         return rootLayout;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button_back) {
+            mNavigator.pop();
+        } else if (id == R.id.button_share) {
+            Context context = view.getContext();
+            UiUtils.shareFile(context, getFile(), context.getString(R.string.share_image));
+        }
+    }
+
+    private File getFile() {
+        Args args = Args.of(mNavRoute);
+        if (args != null) {
+            return new File(args.mAbsolutePath);
+        }
+        return null;
     }
 
     private Uri getFileUri() {
